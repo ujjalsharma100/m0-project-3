49e003eb767423f4f38ed6c06280f56fad9621c1
ujjalsharma100 <ujjalsharma100@gmail.com>
2026-05-11 22:59:11 +0530

    new commit

--- app2.py
+++ app2.py
@@ -1,5 +1,10 @@
 def greet(name: str) -> str:
     return f"Hello, {name}!"
 
+
+def farewell(name: str) -> str:
+    return f"Goodbye, {name}!"
+
+
 if __name__ == "__main__":
     print(greet("app2"))
